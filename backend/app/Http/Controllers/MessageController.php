6889aba0730901a8b0a29a15cd79e9ad27c9130c
ProgrammerJayT <theanthem8@gmail.com>
2024-03-27 14:15:42 +0200

    Add MessageController index and store methods

--- backend/app/Http/Controllers/MessageController.php
+++ backend/app/Http/Controllers/MessageController.php
@@ -2,18 +2,29 @@
 
 namespace App\Http\Controllers;
 
+use App\Events\V1\MessageSent;
+use App\Http\Resources\V1\MessageCollection;
+use App\Http\Resources\V1\MessageResource;
 use App\Models\V1\Message;
 use App\Http\Requests\StoreMessageRequest;
 use App\Http\Requests\UpdateMessageRequest;
+use Illuminate\Http\Request;
 
 class MessageController extends Controller
 {
     /**
      * Display a listing of the resource.
      */
-    public function index()
+    public function index(Request $request)
     {
         //
+        try {
+            return response()->json([
+                'messages' => new MessageCollection(Message::all())
+            ], 200);
+        } catch (\Throwable $th) {
+            //throw $th;
+        }
     }
 
     /**
@@ -30,6 +41,20 @@ class MessageController extends Controller
     public function store(StoreMessageRequest $request)
     {
         //
+        try {
+            $newMessage = Message::create($request->validated());
+
+            event(new MessageSent($newMessage));
+
+            return response([
+                'message' => new MessageResource($newMessage)
+            ], 200);
+        } catch (\Throwable $th) {
+            //throw $th;
+            return response()->json([
+                'message' => $th->getMessage()
+            ], 500);
+        }
     }
 
     /**
