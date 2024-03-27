21c319c054ef3eb1c7ad5ad1e6c5357258a940b2
ProgrammerJayT <theanthem8@gmail.com>
2024-03-27 14:15:17 +0200

    Add ShouldBroadcast interface to MessageSent event class

--- backend/app/Events/V1/MessageSent.php
+++ backend/app/Events/V1/MessageSent.php
@@ -11,7 +11,7 @@ use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
 use Illuminate\Foundation\Events\Dispatchable;
 use Illuminate\Queue\SerializesModels;
 
-class MessageSent
+class MessageSent implements ShouldBroadcast
 {
     use Dispatchable, InteractsWithSockets, SerializesModels;
 
