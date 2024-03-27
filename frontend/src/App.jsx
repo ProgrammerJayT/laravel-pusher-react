c38555afbc06233f3612b2d0c4b773e3bb4d6fcb
ProgrammerJayT <theanthem8@gmail.com>
2024-03-27 14:06:23 +0200

    Refactor

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,35 +1,35 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useState, useEffect } from "react";
+import { pusher } from "./constants/pusher/config";
+import "./App.css";
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+  const [message, setMessage] = useState(null);
+
+  useEffect(() => {
+    const channel = pusher.subscribe("laravel-pusher-react");
+    channel.bind("message-sent", (data) => {
+      console.log("Received data:", data);
+      setMessage(data.message); // Assuming 'messages' is the property containing the message data
+    });
+
+    return () => {
+      // Unsubscribe from Pusher channel when component unmounts
+      pusher.unsubscribe("laravel-pusher-react");
+    };
+  }, []);
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    <div>
+      {message ? (
+        <div>
+          <p>Time: {message.created_at}</p>
+          <p>Message: {message.message}</p>
+        </div>
+      ) : (
+        <div>Waiting...</div>
+      )}
+    </div>
+  );
+};
 
-export default App
+export default App;
