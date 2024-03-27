ea308ebd89e6c1af13c3f93d1fc830bc7325649a
ProgrammerJayT <theanthem8@gmail.com>
2024-03-27 11:09:04 +0200

    Created message sent event

---
+++ backend/app/Events/V1/MessageSent.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Events\V1;
+
+use App\Models\V1\Message;
+use Illuminate\Broadcasting\Channel;
+use Illuminate\Broadcasting\InteractsWithSockets;
+use Illuminate\Broadcasting\PresenceChannel;
+use Illuminate\Broadcasting\PrivateChannel;
+use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
+use Illuminate\Foundation\Events\Dispatchable;
+use Illuminate\Queue\SerializesModels;
+
+class MessageSent
+{
+    use Dispatchable, InteractsWithSockets, SerializesModels;
+
+    public $message;
+
+    /**
+     * Create a new event instance.
+     */
+    public function __construct(Message $message)
+    {
+        //
+        $this->message = $message;
+    }
+
+    /**
+     * Get the channels the event should broadcast on.
+     *
+     * @return array<int, \Illuminate\Broadcasting\Channel>
+     */
+    public function broadcastOn(): array
+    {
+        return [
+            'laravel-pusher-react'
+        ];
+    }
+
+    public function broadcastAs(): string
+    {
+        return 'message-sent';
+    }
+}
